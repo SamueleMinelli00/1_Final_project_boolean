791ec97823e611c95bd5d5ffe5435e3c964ea3dc
Samuele Minelli Zuffa <samuele.minelli.work@gmail.com>
2026-05-27 10:58:43 +0000

    update

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ distances_to_WAP_centroid = gpd.read_file(distances_to_WAP_centroid_path)
 net_distances_to_WAP = gpd.read_file(net_distances_to_WAP_path)
 
 #print(distances_to_WAP_centroid.head(5))
-distances_to_WAP_centroid.info()
+#distances_to_WAP_centroid.info()
 #print(net_distances_to_WAP.head(5))
-net_distances_to_WAP.info()
+#net_distances_to_WAP.info()
 
 ###merging files
 ACLED_to_WAP_merged = distances_to_WAP_centroid.merge(
@@ -41,5 +41,42 @@ ACLED_to_WAP_clean.rename(columns={
    'sub_event_': 'SET',
    'event_id_c': 'event_id'
 }, inplace=True)
-print(ACLED_to_WAP_clean.info())
+#print(ACLED_to_WAP_clean.info())
 
+###convert distances into km (float) with two decimal numbers
+ACLED_to_WAP_clean['distance_to_centroid_km'] = (ACLED_to_WAP_clean['distance_to_centroid_km']/1000).round(2)
+ACLED_to_WAP_clean['distance_to_perimeter_km'] = (ACLED_to_WAP_clean['distance_to_perimeter_km']/1000).round(2)
+#print(ACLED_to_WAP_clean.head(5))
+
+###Filter by A1 and A2 (JNIM)
+ACLED_to_WAP_clean = ACLED_to_WAP_clean[
+    (ACLED_to_WAP_clean['actor1'] == 'JNIM: Group for Support of Islam and Muslims') |
+    (ACLED_to_WAP_clean['actor2'] == 'JNIM: Group for Support of Islam and Muslims')
+    ]
+
+# display(ACLED_to_WAP_clean['actor1'].unique())
+# display(ACLED_to_WAP_clean['actor2'].unique())
+
+#print(ACLED_to_WAP_clean.info())
+
+###Dropping unnecessary Sub Event Types (SETs)
+SET_to_drop = ['Other']
+'''
+I am dropping only other as I am rather interested in all the events involving JNIM whether violent or not.
+The scope of the project is to measure the width of the operations of the group rather than its violent dynamics
+'''
+ACLED_to_WAP_clean = ACLED_to_WAP_clean[~ACLED_to_WAP_clean['SET'].isin(SET_to_drop)]
+
+#print(ACLED_to_WAP_clean['SET'].unique())
+
+###convert event_date to datetime
+ACLED_to_WAP_clean['event_date'] = pd.to_datetime(ACLED_to_WAP_clean['event_date'])
+
+###extract month and calculate trimester
+# Extract month and calculate trimester
+ACLED_to_WAP_clean['month'] = ACLED_to_WAP_clean['event_date'].dt.month
+ACLED_to_WAP_clean['trimester'] = ((ACLED_to_WAP_clean['month'] - 1) // 3) + 1
+ACLED_to_WAP_clean['year_trimester'] = ACLED_to_WAP_clean['event_date'].dt.strftime('%Y') + '-Q' + ACLED_to_WAP_clean['event_date'].dt.quarter.astype(str)
+
+print("DataFrame with 'month', 'trimester', and 'year_trimester' columns:")
+print(ACLED_to_WAP_clean[['event_date', 'month', 'trimester', 'year_trimester']].head())
